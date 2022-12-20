c2947c7dd32804e7f82a3fe52ae50e7631b8c8f1
vikahse <57991510+vikahse@users.noreply.github.com>
2022-12-20 15:09:37 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+from telebot.async_telebot import AsyncTeleBot
+
+TOKEN = '5808740023:AAF8PJXIs4ZTnIACG6otj-H2CCxXsETqnoU'
+bot = AsyncTeleBot(TOKEN)
+
+from handlers import *
+from modules import GameManager
+
+# import os
+# from sqlalchemy import create_engine, Column, Integer, Boolean, String
+# from dotenv import load_dotenv
+# from sqlalchemy.orm import scoped_session, declarative_base, sessionmaker
+#
+# load_dotenv()
+#
+# host = str(os.getenv("HOST"))
+# password = str(os.getenv("PASSWORD"))
+# database = str(os.getenv("DATABASE"))
+#
+# engine = create_engine(f"postgresql+psycopg2://postgres:forever@localhost/bot")
+#
+# session = scoped_session(sessionmaker(bind=engine))
+# Base = declarative_base()
+# Base.query = session.query_property()
+
+manager = GameManager()
+
+if __name__ == '__main__':
+    asyncio.run(bot.polling(non_stop=True))
